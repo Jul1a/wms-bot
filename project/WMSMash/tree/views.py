18eddcde046f0bbde7418f89b33f986a396c62fb
Ju1ia <j.e.makarova@gmail.com>
2010-11-25 17:25:19 +0600

    jquery.treeTable

--- project/WMSMash/tree/views.py
+++ project/WMSMash/tree/views.py
@@ -14,16 +14,13 @@ from django.forms import ModelChoiceField, ChoiceField
 
 
 def show_category_tree(request):
-  #TreeNodeChoiceField
-#  form_server = InForm()
-  #TreeNodeChoiceField
-  selected_server = request.GET.get('list_servers', 0)
-  selected_set = request.GET.get('list_sets', 0)
+
   list_servers = ModelChoiceField(queryset = Servers.objects.all(), 
                                      required = False,
                                      label = '',
                                      widget=forms.Select({'class':'select_style', 
-                                                          'onChange':'this.form.submit();'
+                                                          #'onChange':'this.form.submit();',
+                                                          'id': 'list_servers'
                                                          }),
                                      empty_label = None
                                     ) 
@@ -32,27 +29,45 @@ def show_category_tree(request):
                                   required = False,
                                   label = '',
                                   widget=forms.Select({'class':'select_style', 
-                                                       'onChange':'this.form.submit();'
+                                                       #'onChange':'this.form.submit();'
+                                                        'id': 'list_sets'
                                                       }),
                                   empty_label = None
                                  )
 #  query = request.get_full_path()
-  if not selected_server:
+
+  slt_server = request.GET.get('list_servers', 0)
+  slt_set = request.GET.get('list_sets', 0)
+  if not slt_server:
     for c in Servers.objects.all():
-      selected_server = c.id
+      selected_server = c
       break;
-  if not selected_set:
+    if not selected_server:
+      id_server = None
+    else:
+      id_server = selected_server.id
+  else:
+    selected_server = Servers.objects.get(id=slt_server)
+    id_server = selected_server.id
+  if not slt_set:
     for c in LayerSet.objects.all():
-      selected_set = c.id
+      selected_set = c
       break;
+    if not selected_set:
+      id_set = None
+    else:
+      id_set = selected_set.id
+  else:
+    selected_set = LayerSet.objects.get(id=slt_set)
+    id_set = selected_set.id
   return render_to_response("index.html",
                               { 'MEDIA_URL': MEDIA_URL,
                                 'nodes_layers':Layers.tree.all(), 
                                 'nodes_layertree':LayerTree.tree.all(), 
-                                'list_servers': list_servers.widget.render("list_servers", selected_server),
-                                'list_sets': list_sets.widget.render("list_sets", selected_set),
-                                'selected_server':int(selected_server),
-                                'selected_set':int(selected_set),
+                                'list_servers': list_servers.widget.render("list_servers", id_server),
+                                'list_sets': list_sets.widget.render("list_sets", id_set),
+                                'selected_server':selected_server,
+                                'selected_set':selected_set,
                               },
                               context_instance=RequestContext(request)
                             )
